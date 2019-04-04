07a65e34d6b3ac1b1b4212b5671ee54339e6f963
adwaith-os <adwi@adwi.nitk.ac.in>
2019-04-04 20:59:53 +0530

    Commented the whole script, wrote functions to change colors

--- nasmshell/nasmshell.py
+++ nasmshell/nasmshell.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
+#############################################################################################
+#                                                                                           #
+# nasmshell - A simple python3 wrapper for the awesome nasm and ndisasm commandline tools   #
+# Author: Adwaith Gautham                                                                   #
+# Repo Link: https://github.com/adwait1-G/aasm                                              #
+#                                                                                           #
+# LICENSE: There is no license! Use it the way you want to!                                 #
+#                                                                                           #
+#############################################################################################
+
+
+
+
 import subprocess
 import sys
 import struct
@@ -10,6 +23,14 @@ LISTING_FILE = '/tmp/listing.lst'
 ERROR_FILE = '/tmp/nasm.error'
 DISASM_FILE = '/tmp/disassemble.hex'
 
+#############################################################
+#                                                           #
+# There are 2 important functions:                          #
+#                                                           #
+# 1. Assemble() - Wrapper for nasm                          #
+# 2. Disassemble() - Wrapper for ndisasm                    #
+#                                                           #
+#############################################################
 
 # This function assembles the given Payload. 
 def Assemble(Payload) :
@@ -80,25 +101,47 @@ def Disassemble(Payload) :
         subprocess.run(['rm', DISASM_FILE])
 
 
+#############################################################
+#                                                           #
+# There following increase the fancyness of nasmshell!      #
+#                                                           #
+# 1. changeToYellow() - Changes text color to yellow        #
+# 2. changeToWhite() - Changes text color to white          #
+# 3. changeToRed() - Changes text color to ret              #
+# 1. intro() - A simple intro                               #
+# 2. help() - quick tutorial on how to use nasmshell        #
+#                                                           #
+#############################################################
+
+def changeToRed() : 
+        print("\033[1;31;40m", end='')
+
+def changeToWhite() : 
+        print("\033[1;37;40m", end='')
+        
+def changeToYellow() : 
+        print("\033[1;33;40m", end='')
+
+
+
 def intro() : 
 
-        # Change text color to yello-bold
-        print("\033[1;33;40m")
+        changeToYellow()
         # Intro
         print("Hello!")
         print("\nI am nasmshell! A simple python wrapper for the awesome nasm and ndisasm commandline tools")
+        changeToWhite()
         return
 
 
 # Will print out 1-2 examples of how to use the tool. 
 def help() : 
 
-        # Change text color to yellow-bold
-        print("\033[1;33;40m")
+        changeToYellow()
        
         # Short tutorial
         print("\nThis is how you use me: ")
-        print("\n-->There are 4 commands: ")
+        print("\nThere are 4 commands: ")
         print("\n1. help - Will give you this intro")
         print("\n2. exit - To exit from nasmshell(You can use Ctrl+D also)")
         
@@ -107,24 +150,28 @@ def help() :
         
         # Single instruction example
         print("\n\ta. If you have one single instruction to assemble, you can do it this way: ")
-        print("\t\033[1;31;40mnasmshell>>> \033[1;37;40masm <Inst>")
-        print("\033[1;33;40m")
-        print("\tExample: ")
-        print("\t\033[1;31;40mnasmshell>>> \033[1;37;40masm mov rax, rax")
+        changeToRed()
+        print("\tnasmshell>>> \033[1;37;40masm <Inst>")
+        changeToYellow()
+        print("\n\tExample: ")
+        changeToRed()
+        print("\tnasmshell>>> \033[1;37;40masm mov rax, rax")
         print("\t\t1                                  section .text")
         print("\t\t2                                  	global _start")
         print("\t\t3   ")
         print("\t\t4                                  _start :")
         print("\t\t5 00000000 4889C0                  mov rax, rax")
-        print("\033[1;33;40m")
+        changeToYellow()
         
         # Multiple instruction example
         print("\n\tb. If you have multiple instructions to assemble, you can do it this way: ")
-        print("\t\033[1;31;40mnasmshell>>> \033[1;37;40masm Inst1; Inst2; Inst3; .... InstN")
-        print("\033[1;33;40m")
-        print("\tBasically, separate the instructions by a Semi-colon")
+        changeToRed()
+        print("\tnasmshell>>> \033[1;37;40masm Inst1; Inst2; Inst3; .... InstN")
+        changeToYellow()
+        print("\n\tBasically, separate the instructions by a Semi-colon")
         print("\tExample: ")
-        print("\t\033[1;31;40mnasmshell>>> \033[1;37;40masm mov rax, rax; xor rbx, r15; ret")
+        changeToRed()
+        print("\tnasmshell>>> \033[1;37;40masm mov rax, rax; xor rbx, r15; ret")
         print("\t\t1                                  section .text")
         print("\t\t2                                  	global _start")
         print("\t\t3                      ")
@@ -133,20 +180,33 @@ def help() :
         print("\t\t6 00000003 4C31FB                   xor rbx, r15")
         print("\t\t7 00000006 C3                       ret")
         print("\n")
-        print("\033[1;33;40m")
+        changeToYellow()
 
         # Main command No 2 - disasm
         print("4. disasm - To disassemble valid machine code")
-        print("\t\033[1;31;40mnasmshell>>> \033[1;37;40mdisasm <Valid Machine Code>")
-        print("\033[1;33;40m")
+        changeToRed()
+        print("\tnasmshell>>> \033[1;37;40mdisasm <Valid Machine Code>")
+        changeToYellow()
         print("\n\ta. This is how it's done: ")
-        print("\t\033[1;31;40mnasmshell>>> \033[1;37;40mdisasm 55c34889c0")
+        changeToRed()
+        print("\tnasmshell>>> \033[1;37;40mdisasm 55c34889c0")
         print("\t00000000  55                push rbp")
         print("\t00000001  C3                ret")
         print("\t00000002  4889C0            mov rax,rax")
-        print("\033[1;33;40m")
+
+        changeToWhite()        
+        print("\n\nI hope that helped :)")
 
         return
+
+#############################################################
+#                                                           #
+# The following functions drives nasmshell                  #
+#                                                           #
+# 1. ParseUserInput() - Wrapper for nasm                    #
+# 2. mainloop() - driver function                           #
+#                                                           #
+#############################################################
 
 # The name says it all!
 def ParseUserInput(UserInput) :
@@ -183,7 +243,6 @@ def ParseUserInput(UserInput) :
                 Cmd = UserInput[0 : FirstSpace]
                 Payload = UserInput[FirstSpace+1 : ]
 
-
                 if Cmd == 'asm' :
                         Assemble(Payload)
                 
@@ -191,7 +250,7 @@ def ParseUserInput(UserInput) :
                         Disassemble(Payload)
 
                 else :
-                        print('Unknown Command: ', Cmd)
+                        print('Unknown command: ', Cmd)
         
         return
 
@@ -199,7 +258,8 @@ def ParseUserInput(UserInput) :
 def mainloop() :
     
     while True:
-        print("\033[1;31;40mnasmshell>>> \033[1;37;40m", end='')
+        changeToRed()
+        print("nasmshell>>> \033[1;37;40m", end='')
         try :
                 UserInput = str(input())
         except EOFError :
@@ -219,25 +279,3 @@ if __name__ == "__main__" :
         
         intro()
         mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
